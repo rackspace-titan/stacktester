52dc88fe79d3528d9b5c5d0d6b0f6dcfb6b2bf9d
Brian Lamar <brian.lamar@gmail.com>
2011-06-21 15:45:56 -0400

    Moved README to markdown format Added *.egg-info to .gitignore Bare bones unittests Bare bones bin/stacktester Nosetests running through ./setup.py test

---
+++ tests/test_basic.py
@@ -0,0 +1,15 @@
+"""
+Unit-tests which test the validity of the `stacktester` library.
+
+This file or the directory that it's in should not contain tests which
+test an OpenStack installation.
+"""
+
+import unittest
+
+
+class TestTestsRun(unittest.TestCase):
+    """Sanity check to make sure that unit-tests are being run."""
+
+    def test_works(self):
+        self.assertEquals(True, True)
