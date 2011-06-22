7d9ad0c2865a1818db0a1010f4f1e49ede5a268d
Brian Lamar <brian.lamar@rackspace.com>
2011-06-22 16:21:38 -0400

    Moved pip-requires to tools, removed crap from setup.py, and generally did things with 3 lines of bash that took 20 lines of python.

--- setup.py
+++ setup.py
@@ -1,69 +1,6 @@
 #!/usr/bin/env python
 
-import os.path
-import subprocess
-import sys
-
 import setuptools
-
-
-class BuildVirtualEnvironment(setuptools.Command):
-    """Custom setup.py command which builds the virtual environment."""
-
-    description = "Build a venv containing `stacktester` dependencies."
-    base_dir = os.path.abspath(os.path.normpath(os.path.join(__file__, "..")))
-    user_options = []
-
-    def _create_venv(self):
-        """Actually create the environment, using `virtualenv`."""
-        import virtualenv
-        virtualenv.logger = virtualenv.Logger([])
-
-        print "creating venv in %s" % self.venv_path
-
-        virtualenv.create_environment(
-            self.venv_path,
-            site_packages=False,
-            clear=True,
-        )
-
-    def _install_deps(self):
-        """Install `stacktester` dependencies into the virtual environment."""
-        print "installing required dependencies into %s" % self.venv_path
-        subprocess.call([
-            "pip",
-            "install",
-            "--upgrade",
-            "-E",
-            self.venv_path,
-            "-r",
-            self.pip_req_path,
-        ])
-
-    def _install_stacktester(self):
-        """Install `stacktester` itself into the virtual environment."""
-        print "installing Stacktester into %s" % self.venv_path
-        subprocess.call([
-            "pip",
-            "install",
-            "-E",
-            self.venv_path,
-            self.base_dir,
-        ])
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        """Build the virtual environment."""
-        self.venv_path = os.path.join(self.base_dir, ".venv")
-        self.pip_req_path = os.path.join(self.base_dir, ".pip-requires")
-        self._create_venv()
-        self._install_deps()
-        self._install_stacktester()
 
 
 setuptools.setup(
@@ -72,10 +9,8 @@ setuptools.setup(
     description="Testing suite for OpenStack software suite.",
     author="Rackspace Titan",
     packages=setuptools.find_packages(),
-    setup_requires=["nose", "virtualenv", "pip", "glance"],
-    scripts=["bin/stacktester"],
+    scripts=[
+        "bin/stacktester",
+    ],
     test_suite="nose.collector",
-    cmdclass={
-        "venv": BuildVirtualEnvironment,
-    },
 )
