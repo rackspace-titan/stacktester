6a0e3364a60c1e929785abee8df0143fa6431383
Alex Meade <hatboy112@yahoo.com>
2011-06-28 09:44:49 -0400

    Added tests for server reboot

--- stacktester/tests/test_server_actions.py
+++ stacktester/tests/test_server_actions.py
@@ -16,23 +16,80 @@
 #import random
 #import time
 #
-#from domainobjects import base
-#from domainobjects import openstack
-#import utils
+import stacktester
+from stacktester import openstack
+
+import json
+import unittest2 as unittest
 #
-#
-#class ServerActionsTest(utils.TestCase):
-#
-#    def setUp(self):
-#        self.os = openstack.OpenStack()
-#        self.server = self.os.servers.create(name="testserver",
-#                                image="http://glance1:9292/v1/images/3",
-#                                flavor="http://172.19.0.3:8774/v1.1/flavors/1")
-#        self.server.waitForStatus('ACTIVE')
-#
-#    def tearDown(self):
-#        self.server.delete()
-#
+IMAGE_FIXTURES = [
+    {
+        'name': 'ramdisk',
+        'disk_format': 'ari',
+        'container_format': 'ari',
+        'is_public': True,
+    },
+    {
+        'name': 'kernel',
+        'disk_format': 'aki',
+        'container_format': 'aki',
+        'is_public': True,
+    },
+    {
+        'name': 'image',
+        'disk_format': 'ami',
+        'container_format': 'ami',
+        'is_public': True,
+    },
+]
+
+
+class ServerActionsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.os = openstack.Manager()
+        self.config = stacktester.config.StackConfig()
+
+        self.images = {}
+        for IMAGE_FIXTURE in IMAGE_FIXTURES:
+            IMAGE_FIXTURE['location'] = self.config.glance.get(
+                '%s_uri' % IMAGE_FIXTURE['disk_format'], 'Invalid')
+            meta = self.os.glance.add_image(IMAGE_FIXTURE, None)
+            self.images[meta['name']] = {'id': meta['id']}
+
+        post_body = json.dumps({
+            'server' : {
+                'name' : 'testserver',
+                'imageRef' : self.images['image']['id'],
+                'flavorRef' : 3,
+            }
+        })
+
+        response, body = self.os.nova.request(
+            'POST', '/servers', body=post_body)
+        #self.assertEqual('202', response['status'])
+
+        data = json.loads(body)
+
+        self.server_id = data['server']['id']
+        self.os.nova.wait_for_server_status(self.server_id, 'ACTIVE')
+
+    def tearDown(self):
+        for image in self.images.itervalues():
+            self.os.glance.delete_image(image['id'])
+
+        post_body = json.dumps({
+            'server' : {
+                'name' : 'testserver',
+                'imageRef' : self.images['image']['id'],
+                'flavorRef' : 3,
+            }
+        })
+        response, body = self.os.nova.request(
+            'DELETE',
+            '/servers/%s' % self.server_id,
+            body=body)
+
 #    def test_rebuild_server(self):
 #        """
 #        Test that a server can be rebuilt with a new image
@@ -79,20 +136,41 @@
 #        modified_server = self.os.servers.get(server)
 #        self.assertEqual(new_flavor.name, modified_server.flavorId)
 #
-#    def test_reboot_server(self):
-#        """
-#        Verify that a server can be rebooted
-#        """
-#
-#        self.server.reboot()
-#
-#        #TODO: Need to verify state change
-#
-#    def test_reboot_server_hard(self):
-#        """
-#        Verify that a server can be rebooted
-#        """
-#
-#        self.os.servers.reboot(self.server, type='HARD')
-#
-#        #TODO: Need to verify state change
+
+    def test_reboot_server(self):
+        """
+        Verify that a server can be rebooted
+        """
+
+        post_body = json.dumps({
+            'reboot' : {
+                'type' : 'SOFT',
+            }
+        })
+
+        response, body = self.os.nova.request(
+            'POST', "/servers/%s/action" % self.server_id, body=post_body)
+        self.assertEqual(response['status'], '200')
+
+        #verify state change
+        self.os.nova.wait_for_server_status(self.server_id, 'REBOOT')
+        self.os.nova.wait_for_server_status(self.server_id, 'ACTIVE')
+
+    def test_reboot_server_hard(self):
+        """
+        Verify that a server can be rebooted
+        """
+
+        post_body = json.dumps({
+            'reboot' : {
+                'type' : 'HARD',
+            }
+        })
+
+        response, body = self.os.nova.request(
+            'POST', "/servers/%s/action" % self.server_id, body=post_body)
+        self.assertEqual(response['status'], '200')
+
+        #verify state change
+        self.os.nova.wait_for_server_status(self.server_id, 'HARD_REBOOT')
+        self.os.nova.wait_for_server_status(self.server_id, 'ACTIVE')
