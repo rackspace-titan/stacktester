bfbbd723f01a08ffab8f80452a0b855290cffdcc
William Wolf <throughnothing@gmail.com>
2011-06-30 11:50:29 -0400

    update test_server_meta to use image_ref and flavor_ref configs, and added/updated tests

--- stacktester/tests/test_server_meta.py
+++ stacktester/tests/test_server_meta.py
@@ -21,14 +21,17 @@ from stacktester import openstack
 
 class ServersMetadataTest(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.os = openstack.Manager()
+        self.image_ref = self.os.config.env.image_ref
+        self.flavor_ref = self.os.config.env.flavor_ref
 
         post_body = json.dumps({
             'server' : {
                 'name' : 'testserver',
-                'imageRef' : 3,
-                'flavorRef' : 1,
+                'imageRef' : self.image_ref,
+                'flavorRef' : self.flavor_ref,
                 'metadata' : {
                     'testEntry' : 'testValue'
                 }
@@ -39,16 +42,44 @@ class ServersMetadataTest(unittest.TestCase):
             'POST', '/servers', body=post_body)
 
         data = json.loads(body)
-
         self.server_id = data['server']['id']
-        self.os.nova.wait_for_server_status(self.server_id, 'ACTIVE')
-
-    def tearDown(self):
+    
+    @classmethod
+    def tearDownClass(self):
         self.os.nova.request('DELETE', '/servers/%s' % self.server_id)
 
-    def test_get_servers_metdata(self):
+    def test_get_servers_metadata(self):
         """Test that we can retrieve metadata for a server"""
 
-        response, body = self.os.nova.request('GET', '/servers/%s/meta' % self.server_id)
+        response, body = self.os.nova.request(
+            'GET', '/servers/%s/meta' % self.server_id)
+        result = json.loads(body)['metadata']
+        self.assertEqual(result['testEntry'], 'testValue')
+
+    def test_add_servers_metadata(self):
+        """Test that we can add metadata to a server"""
+
+        expected_meta = {'new_meta1' : 'new_value1', 'new_meta2' : 'new_value2'}
+
+        put_body = json.dumps({
+            'server' : {
+                'metadata' : expected_meta
+            }
+        })
+
+        response, body = self.os.nova.request(
+            'PUT', '/servers/%s' % self.server_id, body=put_body)
+        self.assertEqual(204, response.status)
+
+        response, body = self.os.nova.request(
+            'GET', '/servers/%s/meta' % self.server_id)
+        metadata = json.loads(body)['metadata']
+        self.assertDictEqual(expected_meta, metadata)
+
+    def test_update_servers_metadata(self):
+        """Test that we can update metadata for a server"""
+
+        response, body = self.os.nova.request(
+            'GET', '/servers/%s/meta' % self.server_id)
         result = json.loads(body)['metadata']
         self.assertEqual(result['testEntry'], 'testValue')
